75f07dce8d0bf4f8c0778410b785f96907174315
William Taylor <me@willtaylor.info>
2021-09-12 13:54:42 +0100

    Pre-build all kustomizations together before linting

--- scripts/kustomize-kube-linter.sh
+++ scripts/kustomize-kube-linter.sh
@@ -15,7 +15,13 @@ then
     exit
 fi
 
+compiled_yaml_doc=$(mktemp)
+trap "rm -f $compiled_yaml_doc" 0 2 3 15
+
 for kustomization in "$@"
 do
-    kustomize build $kustomization | kube-linter lint -
+    kustomize build $(dirname $kustomization) >> $compiled_yaml_doc
+    echo -e "\n---" >> $compiled_yaml_doc
 done
+
+kube-linter lint $compiled_yaml_doc
